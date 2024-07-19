023cc1c34fe41d50d68d3834bae0142b006e40c9
Sam Turner <3241671+samst3rhamst3r@users.noreply.github.com>
2024-07-18 19:54:24 -0700

    Removed database functionality - using YAMLs instead

--- src/app/events/event.py
+++
@@ -1,5 +0,0 @@
-import enum
-
-class Event(enum.Enum):
-    AGENDA_REVEALED = enum.auto()
-    SYSTEM_ACTIVATED = enum.auto()
--- src/app/map/system.py
+++
@@ -1,5 +0,0 @@
-import dataclasses as dc
-
-@dc.dataclass(slots=True)
-class System:
-    pass

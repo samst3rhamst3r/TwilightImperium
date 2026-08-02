be705d332201e610396642a6d9ecbef1a4e0a0ae
Sam Turner <3241671+samst3rhamst3r@users.noreply.github.com>
2026-08-01 22:57:56 -0700

    Added functionality to isolate the _instance_id of State Objects. Added a restore method to restore from save file in the future.

--- src/app/state/base.py
+++ src/app/state/base.py
@@ -1,14 +1,29 @@
 from dataclasses import dataclass, field
-from typing import Final
+from typing import Self
 from uuid import uuid4
 
-from app.config.base import BaseConfigObj
+from app.config.base import BaseConfigObj, ConfigObj
 from app.config.text import BaseTextConfigObj
 
 @dataclass(slots=True, kw_only=True)
 class BaseStateObj[TConfig: BaseConfigObj, TTextConfig: BaseTextConfigObj]:
     """Base class for all state objects."""
-    config: Final[TConfig]
-    text_config: Final[TTextConfig]
-    instance_id: Final[str] = field(default_factory=lambda: uuid4().hex)
+    config: TConfig
+    text_config: TTextConfig
+    _instance_id: str = field(init=False, default_factory=lambda: uuid4().hex)
+
+    @property
+    def instance_id(self) -> str:
+        return self._instance_id
     
+    @classmethod
+    def restore(cls, config: TConfig, text_config: TTextConfig, instance_id: str, **kwargs) -> Self:
+        obj = cls(config=config, text_config=text_config, **kwargs)
+        obj._instance_id = instance_id
+        return obj
+
+@dataclass(slots=True, kw_only=True)
+class ConfigIDStateObj[TConfig: ConfigObj, TTextConfig: BaseTextConfigObj](BaseStateObj[TConfig, TTextConfig]):
+
+    def __post_init__(self):
+        self._instance_id = self.config.id
--- src/app/state/planet.py
+++ src/app/state/planet.py
@@ -2,10 +2,11 @@ from dataclasses import dataclass
 from typing import Optional
 
 from app.config.planet import PlanetConfig
+from app.config.text import PlanetTextConfig
 
-from .base import BaseStateObj
+from .base import ConfigIDStateObj
 from .shared.exhaustable import Exhaustable
 
 @dataclass(slots=True, kw_only=True)
-class PlanetState(BaseStateObj[PlanetConfig], Exhaustable):
+class PlanetState(ConfigIDStateObj[PlanetConfig, PlanetTextConfig], Exhaustable):
     is_controlled_by: Optional[str] = None
